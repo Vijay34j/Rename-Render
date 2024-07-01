15ae1108f1a098835ef8d452289f7ae42442f807
Vijay34j <151818969+Vijay34j@users.noreply.github.com>
2024-07-01 15:57:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,14 @@
 
 
 import re, os
+from os import environ
+
+API = environ.get("API", "") # shortlink api
+URL = environ.get("URL", "") # shortlink domain without https://
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
+BOT_USERNAME = environ.get("BOT_USERNAME", "") # bot username without @
+VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
+
 
 id_pattern = re.compile(r'^.\d+$') 
 
